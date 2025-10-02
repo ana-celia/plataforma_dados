3f8bb37e54da860967ea7ee3c3dc2c93758a9f72
Ana Célia Araújo <112821127+ana-celia@users.noreply.github.com>
2025-10-02 13:07:01 +0000

    adiciona demo github actions e alteracoes no backend

--- 01 backend/config/asgi.py
+++ 01 backend/config/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plataforma.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dataplataform.settings')
 
 application = get_asgi_application()
